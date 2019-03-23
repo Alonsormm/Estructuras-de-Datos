ced429043df53fbfb1ab4d8487cf4031ba4cdc01
Alonsormm <alonsorubmaya@gmail.com>
2019-03-23 09:19:48 -0600

    Practica terminada

--- 6/agenda.cpp
+++ 6/agenda.cpp
@@ -79,7 +79,7 @@ Nodo* Query(Nodo* &raiz, string nombre){
         return Query(raiz->right, nombre);
 }
 
-void ficheroLista(Nodo* &raiz){
+void ficheroArbol(Nodo* &raiz){
     fstream txtFile;
     string dato, nombre,direccion, numero, correo;
     vector<string> temp(4);
@@ -96,15 +96,74 @@ void ficheroLista(Nodo* &raiz){
     txtFile.close();
 }
 
+void agregarRegistro(Nodo* &raiz){
+    string nombre,direccion, numero, correo, cadenaFinal;
+    cout << "Dame el nombre completo empezando por nombre: ";
+    cin.ignore(256, '\n');
+    getline(cin, nombre);
+    cout << "Dame la direccion: ";
+    getline(cin, direccion);
+    cout << "Numero de telefono: ";
+    cin >> numero;
+    cout << "Correo Electronico: ";
+    cin >> correo;
+    insert(raiz,nombre,direccion,numero,correo);
+    ofstream archivo;
+    archivo.open("datos_agenda - data.csv",ios::app);
+    cadenaFinal = nombre + ',' + direccion + ',' + numero + ',' + correo + '\n';
+    archivo << cadenaFinal;
+    archivo.close();
+    cout << "El registro de ha agregado correctamente :)\n";
+}
+
+void consultaRegistro(Nodo* &raiz){
+    string nombre;
+    cout << "Escriba el nombre completo del registro que desea buscar: ";
+    cin.ignore(256, '\n');
+    getline(cin, nombre, '\n');
+    Nodo* temp = Query(raiz,nombre);
+    if(temp){
+        cout << "El registro fue encontrado!\n";
+        temp->printNodo();
+        return;
+    }
+    cout << "El registro no existe :c \n";
+}
+
+
+
+void menu(){
+	Nodo* raiz = NULL;
+	ficheroArbol(raiz);
+    char opc;
+    cout << "Bienvenido a la agenda c++" << endl;
+    while(1934){
+        cout << "Ingrese la opcion que desea\n";
+        cout << "1.-Agregar Dato\n2.-Consultar Dato\n3.-Modificar Dato\n4.-Imprimir Agenda\n5.-Salir\n";
+        cin >> opc;
+        if(opc == '1'){
+            agregarRegistro(raiz);
+        }
+        else if(opc == '2'){
+            consultaRegistro(raiz);
+        }
+        else if(opc == '3'){
+            string nombreTemp;
+            cout << "Dame el nombre completo de la entrada que deaseas modificar: ";
+            cin.ignore(256, '\n');
+            getline(cin, nombreTemp);
+        }
+        else if(opc == '4'){
+            inorder(raiz);
+        }
+        else{
+            break;
+        }
+    }
+}
+
+
 
 int main(){
-	Nodo* arbol = NULL;
-	ficheroLista(arbol);
- 
-	//inorder(arbol);
-	Nodo* temp = Query(arbol, "Zenia Giraldez");
-	if(temp){
-		temp -> printNodo();
-	}
-	return 0;
+	menu();
 }
