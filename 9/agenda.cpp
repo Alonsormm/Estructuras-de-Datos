7d683e27709ec3302682171b6d6b09334e33a7e6
Alonso Maya <perusimm@gmail.com>
2019-05-29 18:48:53 -0500

    Antes de generalizar
    Former-commit-id: 03122ddee721f5886b3f603e1edb89e95f1a8b0a

--- 9/agenda.cpp
+++ 9/agenda.cpp
@@ -1,145 +1,169 @@
-#include<iostream>
+#include<iostream> 
+using namespace std; 
+struct BTreeNode{ 
+	int *keys;
+	int t;
+	BTreeNode **C;
+	int n;
+	bool leaf;
+    BTreeNode(int t1, bool leaf1) { 
+    	t = t1; 
+    	leaf = leaf1; 
 
-using namespace std;
+    	keys = new int[2*t-1]; 
+    	C = new BTreeNode *[2*t]; 
 
-template <class T>
-struct Nodo{
-    T *datos;
-    int grado;
-    Nodo **hijos;
-    int actual;
-    bool leaf;
-    Nodo(T _grado, bool _leaf){
-        grado = _grado;
-        leaf = leaf;
-        datos = new T[2*grado-1];
-        hijos = new Nodo* [2*grado];
-        actual = 0;
-    }
-    void imprimir(){
-        int i;
-        for (i = 0; i < actual; i++){
-            if(leaf == false)
-                hijos[i]->imprimir();
-            cout << "" << datos[i];
-        }
-        if(leaf == false)
-            hijos[i]->imprimir();
-        
-    }
-    Nodo* buscar(T dato){
-        int i = 0;
-        while(i < actual && dato > datos[i])
-            i++;
-        if(datos[i == dato]){
-            return this;
-        }
-        return hijos[i]->buscar(dato);
-    }
-
-    void dividirHijos(int i, Nodo<T> *y){
-        Nodo<T>* z = new Nodo(y->grado,y->leaf);
-        z->actual = grado-1;
-
-        for(int j = 0; j < grado-1; j++){
-            z->datos[j] = y->datos[j+grado];
-        }
-        if(y->leaf == false){
-            for (int j = 0; j < grado; j++){
-                z->hijos[j] = y->hijos[j+grado];
-            }
-        }
-        y->actual = grado-1;
-
-        for(int j = actual; j >= i+1; j--)
-            hijos[j+1] = hijos[j];
-        hijos[i+1] = z;
-
-        for(int j = actual-1; j>=i; j--){
-            datos[j+1] = datos[j];
-        }
-        datos[j] = y->datos[t-1];
-        n++;
-    }
-
-    void insertNonFull(T k) { 
-    int i = actual-1; 
-  
-    if (leaf == true){ 
-        while (i >= 0 && datos[i] > k) 
-        { 
-            datos[i+1] = datoas[i]; 
-            i--; 
-        } 
-  
-        datos[i+1] = k; 
-        actual = actual+1; 
+    	n = 0; 
     } 
-    else{ 
-        while (i >= 0 && datos[i] > k) 
-            i--; 
-  
-        if (hijos[i+1]->actual == 2*grado-1){ 
-            dividirHijos(i+1, hijos[i+1]); 
-  
-            if (datos[i+1] < k) 
-                i++; 
-        } 
-        hijos[i+1]->insertNonFull(k); 
+
+    void insertNonFull(int k) { 
+    	int i = n-1; 
+
+    	if (leaf == true){ 
+    		while (i >= 0 && keys[i] > k){ 
+    			keys[i+1] = keys[i]; 
+    			i--; 
+    		} 
+
+    		keys[i+1] = k; 
+    		n = n+1; 
+    	} 
+    	else{ 
+    		while (i >= 0 && keys[i] > k) 
+    			i--; 
+
+    		if (C[i+1]->n == 2*t-1){ 
+    			splitChild(i+1, C[i+1]); 
+
+    			if (keys[i+1] < k) 
+    				i++; 
+    		} 
+    		C[i+1]->insertNonFull(k); 
+    	} 
     } 
+
+    void splitChild(int i, BTreeNode *y){ 
+    	BTreeNode *z = new BTreeNode(y->t, y->leaf); 
+    	z->n = t - 1; 
+
+    	for (int j = 0; j < t-1; j++) 
+    		z->keys[j] = y->keys[j+t]; 
+
+    	if (y->leaf == false){ 
+    		for (int j = 0; j < t; j++) 
+    			z->C[j] = y->C[j+t]; 
+    	} 
+
+    	y->n = t - 1; 
+
+    	for (int j = n; j >= i+1; j--) 
+    		C[j+1] = C[j]; 
+
+    	C[i+1] = z; 
+
+    	for (int j = n-1; j >= i; j--) 
+    		keys[j+1] = keys[j]; 
+
+    	keys[i] = y->keys[t-1]; 
+
+    	n = n + 1; 
+    }  
+
+	void traverse(){ 
+	    int i; 
+	    for (i = 0; i < n; i++){ 
+	    	if (leaf == false) 
+	    		C[i]->traverse(); 
+	    	cout << " " << keys[i]; 
+	    } 
+
+	    if (leaf == false) 
+	    	C[i]->traverse(); 
+    }  
+
+	BTreeNode *search(int k){ 
+	    int i = 0; 
+	    while (i < n && k > keys[i]) 
+	    	i++; 
+	    if (keys[i] == k) 
+	    	return this; 
+	    if (leaf == true) 
+	    	return NULL; 
+	    return C[i]->search(k); 
+    } 
+ 
+
+}; 
+
+struct BTree{ 
+	BTreeNode *root;
+	int t;
+	BTree(int _t){
+        root = NULL;
+        t = _t;
+    } 
+
+	void traverse(){
+        if (root != NULL) root->traverse();
+    } 
+
+	BTreeNode* search(int k){
+        return (root == NULL)? NULL : root->search(k);
+    } 
+
+	void insert(int k){ 
+	    if (root == NULL){ 
+	    	root = new BTreeNode(t, true); 
+	    	root->keys[0] = k; 
+	    	root->n = 1;
+	    } 
+	    else{ 
+	    	if (root->n == 2*t-1){ 
+	    		BTreeNode *s = new BTreeNode(t, false); 
+
+	    		s->C[0] = root; 
+
+	    		s->splitChild(0, root); 
+
+	    		int i = 0; 
+	    		if (s->keys[0] < k) 
+	    			i++; 
+	    		s->C[i]->insertNonFull(k); 
+
+	    		root = s; 
+	    	} 
+	    	else
+	    		root->insertNonFull(k); 
+	    } 
+    } 
+}; 
+
+
+
+
+
+
+
+int main() 
+{ 
+	BTree t(4);
+	t.insert(10); 
+	t.insert(20); 
+	t.insert(5); 
+	t.insert(6); 
+	t.insert(12); 
+	t.insert(30); 
+	t.insert(7); 
+	t.insert(17); 
+
+	cout << "Traversal of the constucted tree is "; 
+	t.traverse(); 
+
+	int k = 6; 
+	(t.search(k) != NULL)? cout << "\nPresent" : cout << "\nNot Present"; 
+
+	k = 15; 
+	(t.search(k) != NULL)? cout << "\nPresent" : cout << "\nNot Present"; 
+
+	return 0; 
 } 
-
-};
-template <class T>
-struct BTree{
-    Nodo<T> *raiz;
-    int grado;
-    BTree(int _grado){
-        grado = _grado;
-        raiz = NULL;
-    }
-    void imprimir(){
-        if(raiz!=NULL)
-            raiz->imprimir();
-    }
-    void insert(T dato){
-        if(raiz == NULL){
-            raiz = new Nodo<T>(grado,false);
-            raiz->datos[0] = dato;
-            raiz->actual = 1;
-        }
-        else{
-            if(raiz -> n == 2*grado-1){
-                Nodo<T>* temp = new Nodo<T>(grado,false);
-                temp->datos[0] = raiz;
-                temp->dividirHijos(0,raiz);
-                int i = 0;
-                if(temp->datos[0] < dato)
-                    i++;
-                temp -> hijos[i]->insertNoLleno(dato);
-
-                raiz = temp;
-            }
-            else{
-                raiz->insertNoLleno(dato);
-            }
-        }
-
-    }
-    
-
-};
-
-int main(){
-    BTree<int> a(3);
-    a.insert(10); 
-    a.insert(20); 
-    a.insert(5); 
-    a.insert(6); 
-    a.insert(12); 
-    a.insert(30); 
-    a.insert(7); 
-    a.insert(17);
-
-    a.imprimir();
-}
